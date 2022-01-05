1fe91083fbe75c8802ec35dc15e847636ebb4c2a
kirbyneaton <kjneat@gmail.com>
2022-01-05 14:03:14 -0500

    Start instert into table

--- import_db.sql
+++ import_db.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS question_follows;
 DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS users;
 
 PRAGMA foreign_keys = ON;
@@ -16,10 +18,11 @@ CREATE TABLE users (
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-
 
 --join table
 CREATE TABLE question_follows (
@@ -36,10 +39,34 @@ CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     parent_reply_id INTEGER,
-    user_id INTEGER,
+    user_id INTEGER NOT NULL,
     body TEXT NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Yaakov', 'Zaetz'),
+    ('Kirby', 'Neaton');
+
+INSERT INTO
+    questions (title, body, user_id)
+VALUES
+    ('Help', 'Cancel my subscription',(SELECT id FROM users WHERE fname = 'Yaakov')),
+    ('Maintenance', 'Elevator not working', (SELECT id FROM users WHERE fname = 'Kirby'));
