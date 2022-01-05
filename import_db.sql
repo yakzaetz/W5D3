3b5819b51a61f00ae0284484a2e9b2d4cee04c8d
kirbyneaton <kjneat@gmail.com>
2022-01-05 11:50:19 -0500

    finish join table

--- import_db.sql
+++ import_db.sql
@@ -4,9 +4,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
-    question_id INTEGER NOT NULL
-
-    
+    -- question_id INTEGER NOT NULL    
 );
 
 DROP TABLE IF EXISTS questions;
@@ -15,13 +13,20 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL
-)
+
+);
 
 
 DROP TABLE IF EXISTS question_follows;
 
+--join table
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    users_id = users.id
-)
+    user_id INTEGER,
+    question_id INTEGER,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
