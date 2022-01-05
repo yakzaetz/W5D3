128442b2586179655ff5869210d6eee7040293e5
kirbyneaton <kjneat@gmail.com>
2022-01-05 16:31:45 -0500

    Start find_by_id methods

--- aa_questions.rb
+++ aa_questions.rb
@@ -15,10 +15,89 @@ end
 
 class Users
     def self.find_by_id(id)
-        id = QuestionsDatabase.instance.execute("SELECT id FROM users")
-    end 
+        user = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless user.length > 0
+
+        Users.new(user.first) # user is stored in an array!
+    end
+
+    def self.find_by_name(fname, lname)
+        
+    end
+
 end
 
 class Questions
-    def find_by_id
+    def self.find_by_id(id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                id = ?
+        SQL
+        return nil unless question.length > 0
+
+        Questions.new(question.first) # question is stored in an array!
+    end
 end 
+
+class QuestionFollows
+    def self.find_by_id(id)
+        question_follow = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_follow.length > 0
+
+        QuestionFollows.new(question_follow.first) # question_follow is stored in an array!
+    end
+
+end
+
+
+class Replies
+    def self.find_by_id(id)
+        reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                replys
+            WHERE
+                id = ?
+        SQL
+        return nil unless reply.length > 0
+
+        Replies.new(reply.first) # reply is stored in an array!
+    end
+
+end
+
+class QuestionLikes
+    def self.find_by_id(id)
+        question_like = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                question_likes
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_like.length > 0
+
+        QuestionLikes.new(question_like.first) # question_like is stored in an array!
+    end
+
+end
